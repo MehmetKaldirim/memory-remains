d68bddba14012299e366116bde4550a53cf83bc1
Mehmet <mathdenizi@gmail.com>
2024-04-14 06:12:47 +0200

    user compoenents basic added with some styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,24 @@
 import React from "react";
+import UserList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      places: 3,
+    },
+    {
+      id: "u2",
+      name: "Math Mathias",
+      image:
+        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      places: 5,
+    },
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
