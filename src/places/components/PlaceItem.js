cff1a5d99b853381615c4f5d4e870d79dedba9c8
Mehmet <mathdenizi@gmail.com>
2024-04-18 19:42:53 +0200

    google map funtionality added and work properly

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
@@ -10,6 +11,7 @@ const PlaceItem = (props) => {
 
   const openMapHandler = () => setShowMap(true);
   const closeMapHandler = () => setShowMap(false);
+  //<h2>THE MAP!</h2>
   return (
     <>
       <Modal
@@ -21,7 +23,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
-          <h2>THE MAP!</h2>
+          <Map center={props.coordinates} zoom={16} />
         </div>
       </Modal>
       <li className="place-item">
