f3eb32c8510bb4c915db84b42e6e333811f97b95
Mehmet <mathdenizi@gmail.com>
2024-04-28 06:04:55 +0200

    custom http request hook added and used

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,29 +1,37 @@
-import React from "react";
-import UserList from "../components/UsersList";
+import React, { useEffect, useState } from "react";
+
+import UsersList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook.js";
 
 const Users = () => {
-  const USERS = [
-    {
-      id: "u1",
-      name: "Math Mathias",
-      image: "https://avatars.githubusercontent.com/u/45769545?s=96&v=4",
-      places: 1,
-    },
-    {
-      id: "u2",
-      name: "Max Schwarz",
-      image:
-        "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-      places: 1,
-    },
-    {
-      id: "u3",
-      name: "Math Mathiass",
-      image: "https://avatars.githubusercontent.com/u/45769545?s=96&v=4",
-      places: 0,
-    },
-  ];
-  return <UserList items={USERS} />;
-};
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedUsers, setLoadedUsers] = useState();
 
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const responseData = await sendRequest(
+          "http://localhost:5001/api/users"
+        );
+
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
+    };
+    fetchUsers();
+  }, [sendRequest]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+    </React.Fragment>
+  );
+};
 export default Users;
