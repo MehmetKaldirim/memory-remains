730270ef319bc92d55d2dce8f512e21f639ccf1b
Mehmet <mathdenizi@gmail.com>
2024-05-02 21:30:45 +0200

    environmental variable added

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -8,35 +8,6 @@ import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import { useHttpClient } from "../../shared/hooks/http-hook";
 
-// const DUMMY_PLACES = [
-//   {
-//     id: "p1",
-//     title: "Empire State Building",
-//     description: "One of the most famous sky scrapers in the world!",
-//     imageUrl:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-//     address: "20 W 34th St, New York, NY 10001",
-//     location: {
-//       lat: 40.7484405,
-//       lng: -73.9878584,
-//     },
-//     creator: "u1",
-//   },
-//   {
-//     id: "p2",
-//     title: "His Empire State Building",
-//     description: "One of the most famous sky scrapers in the world!",
-//     imageUrl:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-//     address: "20 W 34th St, New York, NY 10001",
-//     location: {
-//       lat: 40.7484405,
-//       lng: -73.9878584,
-//     },
-//     creator: "u2",
-//   },
-// ];
-
 const UserPlaces = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedPlaces, setLoadedPlaces] = useState();
@@ -46,7 +17,7 @@ const UserPlaces = () => {
     const fetchPlaces = async () => {
       try {
         const responseData = await sendRequest(
-          "http://localhost:5001/api/places/user/" + userId
+          `${process.env.REACT_APP_BACKEND_URL}/places/user/${userId}`
         );
         setLoadedPlaces(responseData.places);
         console.log(responseData);
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -69,8 +69,9 @@ const Auth = () => {
 
     if (isLoginMode) {
       try {
+        console.log(process.env.REACT_APP_BACKEND_URL);
         const responseData = await sendRequest(
-          "http://localhost:5001/api/users/login",
+          `${process.env.REACT_APP_BACKEND_URL}/users/login`,
           "POST",
           JSON.stringify({
             email: formState.inputs.email.value,
@@ -91,7 +92,7 @@ const Auth = () => {
         formData.append("image", formState.inputs.image.value);
 
         const responseData = await sendRequest(
-          "http://localhost:5001/api/users/signup",
+          process.env.REACT_APP_BACKEND_URL + "/users/signup",
           "POST",
           formData
         );
